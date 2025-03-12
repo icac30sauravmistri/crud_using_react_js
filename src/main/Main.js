31d453f28ee18dd2addba03e163b52cd33b41218
icac30sauravmistri <icac30sauravmistri@gmail.com>
2025-03-12 22:59:52 +0530

    main component/view is created

---
+++ src/main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <div>
+            <div class="welcome">Welcome to our react site.</div>
+        </div>
+    )
+}
+
+export default Main
