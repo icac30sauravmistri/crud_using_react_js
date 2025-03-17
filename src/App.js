e5ad66969e3f98bb83fd9147476f5f498331f35f
icac30sauravmistri <icac30sauravmistri@gmail.com>
2025-03-17 20:35:49 +0530

    all the products routes and pages code done with exact path

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./main/Main";
-// import Products from "./admin/Products";
-// import ProductCreate from "./admin/ProductCreate";
-// import ProductEdit from "./admin/ProductEdit";
+import Products from "./admin/Products";
+import ProductCreate from "./admin/ProductCreate";
+import ProductEdit from "./admin/ProductEdit";
 
 
 const App = () => {
@@ -11,7 +11,10 @@ const App = () => {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Main/>} />
+          <Route path="/" exact element={<Main/>} />
+          <Route path="/admin/products" exact element={<Products/>} />
+          <Route path="/admin/products/create" exact element={<ProductCreate/>} />
+          <Route path="/admin/products/:id/edit" exact element={<ProductEdit/>} />
         </Routes>
       </Router>
     </div>
