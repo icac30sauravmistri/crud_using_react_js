bc23e0118aa32d139ce95a607449bab241574e59
icac30sauravmistri <icac30sauravmistri@gmail.com>
2025-03-17 23:52:03 +0530

    products listing code is done

--- src/admin/Products.js
+++ src/admin/Products.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Wrapper from './Wrapper'
 
 function Products() {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3004/products')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
     return (
         <Wrapper>
-            Products
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Image</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map(p => {
+                        return (
+                            <tr key={p.id}>
+                                <td>{p.id}</td>
+                                <td>{p.title}</td>
+                                <td>
+                                    <img src={p.image} alt={p.title} width={90} />
+                                </td>
+                                <td>
+                                    <button type='submit' name='edit'>Edit</button>
+                                    <button type='submit' name='delete'>Delete</button>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         </Wrapper>
     )
 }
